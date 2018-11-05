c8b20441b2250897c107fc559ac3145ecbc86ccb
Marshall Jiang <marshall.jiang@gmail.com>
2018-11-04 20:27:51 -0500

    Added sin for h(x)

--- mycode.py
+++ mycode.py
@@ -1,3 +1,5 @@
+import math
+
 def f(x):
     """Add a new comment"""
     return 5*x*x + 1
@@ -6,6 +8,11 @@ def g(x):
     """The greatest function ever"""
     return -x
 
+def h(x):
+    return math.sin(x)
+
+
 if __name__ == "__main__":
     print(g(f(2)))
+    print(h(1))
     print("Hello world")
