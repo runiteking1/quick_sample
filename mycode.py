c39d57d59911661d6d78d2fd959d07585e33e9fe
Marshall Jiang <marshall.jiang@gmail.com>
2018-11-04 20:16:12 -0500

    Added g(x)

--- mycode.py
+++ mycode.py
@@ -2,6 +2,10 @@ def f(x):
     """Add a new comment"""
     return 5*x*x + 1
 
+def g(x):
+    """The greatest function ever"""
+    return -x
+
 if __name__ == "__main__":
-    print(f(2))
+    print(g(f(2)))
     print("Hello world")
